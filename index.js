59d3cdea713c5e0371a7486b3f12b6157c5c6795
sumit-linearloop <sumit@linearloop.io>
2024-09-30 13:13:57 +0530

    name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Sumit !');
+  res.send('Hello, DevOps !');
 });
 
 app.listen(port, () => {
